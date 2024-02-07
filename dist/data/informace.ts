16879cf6d314de8aac1eeb57ff25303307330c68
suchomelova-dana <danka.suchomelova@seznam.cz>
2024-02-07 19:30:46 +0100

    build test

---
+++ dist/data/informace.ts
@@ -0,0 +1,76 @@
+import {Information} from "../../src/interfaces/interfaces";
+
+const informace : Information[] = [
+    {
+        date: '25.8.2023',
+        pdf: 'informace 25.8.2023.pdf',
+        description: '',
+    },
+    {
+        date: '8.9.2023',
+        pdf: 'informace 8_9_2023.pdf',
+        description: '',
+    },
+    {
+        date: '15.9.2023',
+        pdf: 'informace 15.9.2023.pdf',
+        description: '',
+    },
+    {
+        date: '21.9.2023',
+        pdf: 'informace 21_9_2023 - pro P a PP.pdf',
+        description: 'pro P a PP',
+    },
+    {
+        date: '10.10.2023',
+        pdf: 'informace 10_10_2023.pdf',
+        description: '',
+    },
+    {
+        date: '17.10.2023',
+        pdf: 'informace 17_10_2023.pdf',
+        description: '',
+    },
+    {
+        date: '5.11.2023',
+        pdf: 'informace 5.11.2023.pdf',
+        description: '',
+    },
+    {
+        date: '13.11.2023',
+        pdf: 'informace 13.11.2023.pdf',
+        description: '',
+    },
+    {
+        date: '16.11.2023',
+        pdf: 'informace 16.11.2023.pdf',
+        description: '',
+    },
+    {
+        date: '16.11.2023',
+        pdf: 'informace 16.11.2023-přípravky.pdf',
+        description: 'přípravky',
+    },
+    {
+        date: '8.1.2024',
+        pdf: 'informace 8.1.2024.pdf',
+        description: '',
+    },
+    {
+        date: '9.1.2024',
+        pdf: 'informace 9.1.2024-PŘÍPRAVKA-2018.pdf',
+        description: 'PŘÍPRAVKA-2018',
+    },
+    {
+        date: '9.1.2024',
+        pdf: 'informace 9.1.2024-PŘÍPRAVKA-2019 A MLADŠÍ.pdf',
+        description: 'PŘÍPRAVKA-2019 A MLADŠÍ',
+    },
+    {
+        date: '29.1.2024',
+        pdf: 'informace 29.1.2024.pdf',
+        description: '',
+    },
+]
+
+export default informace
