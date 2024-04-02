6b838def7583624e15084b924ecd4e0779f787da
suchomelova-dana <danka.suchomelova@seznam.cz>
2024-04-02 22:23:53 +0200

    new deploy

---
+++ scripts/gh-pages-deploy.js
@@ -0,0 +1,25 @@
+const execa = require("execa");
+const fs = require("fs");
+
+(async () => {
+    try {
+        await execa("git", ["checkout", "--orphan", "gh-pages"]);
+        await execa("echo", ["sktrasko.cz", ">","CNAME"]);
+        console.log("Building...");
+        await execa("npm", ["run", "build"]);
+        // Understand if it's dist or build folder
+        const folderName = fs.existsSync("dist") ? "dist" : "build";
+        fs.writeFileSync(`${folderName}/CNAME`, "sktrasko.cz");
+        await execa("git", ["--work-tree", folderName, "add", "--all"]);
+        await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+        console.log("Pushing to gh-pages...");
+        await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+        // await execa("rm", ["-r", folderName]);
+        await execa("git", ["checkout", "-f", "main"]);
+        await execa("git", ["branch", "-D", "gh-pages"]);
+        console.log("Successfully deployed");
+    } catch (e) {
+        console.log(e.message);
+        process.exit(1);
+    }
+})();
