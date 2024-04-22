695a93e9b8a788673b1c0256f96fa0ad89ea96e3
admin-trasko <167458084+admin-trasko@users.noreply.github.com>
2024-04-22 15:28:43 +0200

    Update aktuality.ts

--- public/data/aktuality.ts
+++ public/data/aktuality.ts
@@ -2,11 +2,6 @@ import {Aktualita} from "../../src/interfaces/interfaces";
 
 const aktuality : Aktualita[] = [
     {
-        title: 'Srdečně Vás zveme na 22. ročník závodu jednotlivkyň v moderní gymnastice',
-        PDFtitle: 'O pohár města Vyškova',
-        PDF: 'O pohar mesta Vyskova 2024.pdf'
-    },
-    {
         title: 'Přebor jihomoravské soutěžní odblasti NML A a NST A',
         PDFtitle: 'časový program',
         PDF: 'OP NML a NST-casovy program.pdf'
