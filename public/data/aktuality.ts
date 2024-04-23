7dd3b15b226a4f0665277c2277e178a775d6297b
admin-trasko <167458084+admin-trasko@users.noreply.github.com>
2024-04-23 09:18:39 +0200

    Update aktuality.ts

--- public/data/aktuality.ts
+++ public/data/aktuality.ts
@@ -1,10 +1,15 @@
 import {Aktualita} from "../../src/interfaces/interfaces";
 
 const aktuality : Aktualita[] = [
-    {
+        {
         title: 'Přebor jihomoravské soutěžní odblasti NML A a NST A',
         PDFtitle: 'časový program',
         PDF: 'OP NML a NST-casovy program.pdf'
+    },
+    {
+        title: 'Přebor jihomoravské soutěžní odblasti NML A a NST A',
+        PDFtitle: 'startovní listiny',
+        PDF: 'OP NML A - startovka.pdf'
     },
     
 
