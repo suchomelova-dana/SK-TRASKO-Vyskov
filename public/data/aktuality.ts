9fe3bac2bb09a1fe3384e683c33128040e0845c4
admin-trasko <167458084+admin-trasko@users.noreply.github.com>
2024-05-07 11:46:31 +0200

    Update aktuality.ts

--- public/data/aktuality.ts
+++ public/data/aktuality.ts
@@ -2,10 +2,15 @@ import {Aktualita} from "../../src/interfaces/interfaces";
 
 const aktuality : Aktualita[] = [
         {
-        title: '29. 4. Informace pro členy',
+        title: 'Pozvánka na závod Traskáček',
+        PDFtitle: 'rozpis závodu',
+        PDF: 'Traskacek 2024.pdf'
+        },
+        {
+        title: '4. 5. Informace pro členy',
         PDFtitle: '',
         PDF: ''
-    },
+        },
      
 
 ]
