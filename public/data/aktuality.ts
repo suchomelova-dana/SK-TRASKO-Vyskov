fe9daf525a1d639f5574976d699592ea5890f9fd
admin-trasko <167458084+admin-trasko@users.noreply.github.com>
2024-06-18 11:19:29 +0200

    Update aktuality.ts

--- public/data/aktuality.ts
+++ public/data/aktuality.ts
@@ -10,6 +10,6 @@ const aktuality : Aktualita[] = [
 
 ]
 
-export const aktualityImage  = "Pozvanka akademie 2024.jpg";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
+export const aktualityImage  = "";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
 
 export default aktuality
