4c5e1e40b11af4fe73ccbe1668ff76094c45836e
suchomelova-dana <danka.suchomelova@seznam.cz>
2024-06-18 09:20:06 +0000

    gh-pages

---
+++ data/aktuality.ts
@@ -0,0 +1,15 @@
+import {Aktualita} from "../../src/interfaces/interfaces";
+
+const aktuality : Aktualita[] = [
+        {
+                title: '',
+                PDFtitle: '',
+                PDF: ''
+        },
+     
+
+]
+
+export const aktualityImage  = "";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
+
+export default aktuality
