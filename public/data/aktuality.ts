41fadeafef6a4a9b7cae65db0ffd401899fd4fef
admin-trasko <167458084+admin-trasko@users.noreply.github.com>
2024-09-13 08:55:01 +0200

    Update aktuality.ts

--- public/data/aktuality.ts
+++ public/data/aktuality.ts
@@ -2,7 +2,7 @@ import {Aktualita} from "../../src/interfaces/interfaces";
 
 const aktuality : Aktualita[] = [
         {
-                title: 'TRASKO Cup 2024',
+                title: 'Pozvánka na závod ve společných skladbách  ',
                 PDFtitle: 'TRASKO CUP 2024 - rozpis.pdf',
                 PDF: ''
         },
