a4c7826e4969ce3cb0869d288051d68f10de023d
suchomelova-dana <danka.suchomelova@seznam.cz>
2024-10-10 09:12:00 +0000

    gh-pages

---
+++ data/aktuality.ts
@@ -0,0 +1,15 @@
+import {Aktualita} from "../../src/interfaces/interfaces";
+
+const aktuality : Aktualita[] = [
+        {
+                title: 'Pozvánka na závod ve společných skladbách:  ',
+                PDFtitle: 'TRASKO CUP 2024 - rozpis.pdf',
+                PDF: ''
+        },
+     
+
+]
+
+export const aktualityImage  = "";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
+
+export default aktuality
---
+++ data/clenkySA.ts
@@ -0,0 +1,16 @@
+import {Member} from "../../src/interfaces/interfaces";
+
+const SAmembers : Member[] = [
+    { name: 'Frydrychová Eliška' },
+    { name: 'Holubová Kateřina' },
+    { name: 'Matoušková Patricie' },
+    { name: 'Mikešová Alexandra' },
+    { name: 'Novosadová Evelin' },
+    { name: 'Sochorová Zuzana' },
+    { name: 'Šlapakova Apolena' },
+    { name: 'Šmakalová Iveta' },
+    { name: 'Vágnerová Linda' },
+    { name: 'Vejrostová Laura' },
+]
+
+export default SAmembers
