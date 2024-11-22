392f7e94b71e469bf1d48580bc9400cfc66e6ec2
admin-trasko <167458084+admin-trasko@users.noreply.github.com>
2024-11-22 14:30:21 +0100

    Update aktuality.ts

--- public/data/aktuality.ts
+++ public/data/aktuality.ts
@@ -3,13 +3,13 @@ dkuimport {Aktualita} from "../../src/interfaces/interfaces";
 const aktuality : Aktualita[] = [
         {
                 title: 'Pozvánka na vánoční besídku:  ',
-                PDFtitle: 'Pozvánka besídka 2024.jpg',
+                PDFtitle: '',
                 PDF: ''
         },
      
 
 ]
 
-export const aktualityImage  = "";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
+export const aktualityImage  = "Pozvánka besídka 2024.jpg";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
 
 export default aktuality
