c80ba1ad9e8b4c549ea24c578b4dff82b0cb1e66
suchomelova-dana <danka.suchomelova@seznam.cz>
2024-11-27 13:53:28 +0000

    gh-pages

---
+++ data/aktuality.ts
@@ -0,0 +1,15 @@
+import {Aktualita} from "../../src/interfaces/interfaces";
+
+const aktuality : Aktualita[] = [
+        {
+                title: 'Historický úspěch našich gymnastek - družstvo juniorek ESG skončilo na Mistrovství světa v Estonsku na 11. místě!  ',
+                PDFtitle: 'výsledky MS',
+                PDF: 'MS ESG 2024.pdf'
+        },
+     
+
+]
+
+export const aktualityImage  = "Pozvánka besídka 2024.jpg";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
+
+export default aktuality
