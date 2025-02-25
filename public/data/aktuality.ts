b54fd4604001a9be80231de7fe041ddcca4ca1df
admin-trasko <167458084+admin-trasko@users.noreply.github.com>
2025-02-25 09:23:43 +0100

    Update aktuality.ts

--- public/data/aktuality.ts
+++ public/data/aktuality.ts
@@ -2,9 +2,9 @@ import {Aktualita} from "../../src/interfaces/interfaces";
 
 const aktuality : Aktualita[] = [
         {
-                title: 'Historický úspěch našich gymnastek - družstvo juniorek ESG skončilo na Mistrovství světa v Estonsku na 11. místě!  ',
-                PDFtitle: 'výsledky MS',
-                PDF: 'MS ESG 2024.pdf'
+                title: 'O pohár města Vyškova - pozvánka a rozpis závodu',
+                PDFtitle: 'rozpis',
+                PDF: 'O pohar mesta Vyskova 2025.pdf'
         },
         
      
