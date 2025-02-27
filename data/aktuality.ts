fc19df8e8fabfc3118f2eec69df847fe5689d701
suchomelova-dana <danka.suchomelova@seznam.cz>
2025-02-27 07:57:20 +0000

    gh-pages

---
+++ data/aktuality.ts
@@ -0,0 +1,16 @@
+import {Aktualita} from "../../src/interfaces/interfaces";
+
+const aktuality : Aktualita[] = [
+        {
+                title: 'O pohár města Vyškova - ',
+                PDFtitle: 'pozvánka a rozpis závodu',
+                PDF: 'O pohar mesta Vyskova 2025.pdf'
+        },
+        
+     
+
+]
+
+export const aktualityImage  = "";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
+
+export default aktuality
