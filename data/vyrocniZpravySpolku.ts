63334f8ae5b1c6534a7517ac325eed340c4f647d
suchomelova-dana <danka.suchomelova@seznam.cz>
2025-03-17 07:56:25 +0000

    gh-pages

---
+++ data/vyrocniZpravySpolku.ts
@@ -0,0 +1,43 @@
+import {VyrocniZprava} from "../../src/interfaces/interfaces";
+
+const vyrocniZpravySpolku : VyrocniZprava[] = [
+    {
+        year: 2015,
+        pdf: 'vyrocni_zp_2015.pdf'
+    },
+    {
+        year: 2016,
+        pdf: 'vyrocni_zp_2016.pdf'
+    },
+    {
+        year: 2017,
+        pdf: 'vyrocni_zp_2017.pdf'
+    },
+    {
+        year: 2018,
+        pdf: 'vyrocni_zp_2018.pdf'
+    },
+    {
+        year: 2019,
+        pdf: 'vyrocni_zp_2019.pdf'
+    },
+    {
+        year: 2020,
+        pdf: 'vyrocni_zp_2020.pdf'
+    },
+    {
+        year: 2021,
+        pdf: 'vyrocni_zp_2021.pdf'
+    },
+    {
+         year: 2022,
+         pdf: 'Výroční zpráva za rok 2022.pdf'
+    },
+    {
+         year: 2023,
+         pdf: 'Výroční zpráva SK TRASKO Vyškov za rok 2023.pdf'
+    },
+    
+]
+
+export default vyrocniZpravySpolku
