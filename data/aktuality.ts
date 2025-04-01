536eaf84fc66655cc6db2eb6c3f00640619aed88
suchomelova-dana <danka.suchomelova@seznam.cz>
2025-04-01 08:10:54 +0000

    gh-pages

---
+++ data/aktuality.ts
@@ -0,0 +1,25 @@
+import {Aktualita} from "../../src/interfaces/interfaces";
+
+const aktuality : Aktualita[] = [
+        {
+                title: 'O pohár města Vyškova ',
+                PDFtitle: 'pozvánka a rozpis závodu',
+                PDF: 'O pohar mesta Vyskova 2025.pdf'
+        },
+        {
+                title: 'O pohár města Vyškova ',
+                PDFtitle: 'časový program a rozcvičení',
+                PDF: 'O pohar mesta Vyskova 2025 - časový program.pdf'
+        },
+        {
+                title: 'Traskáček ',
+                PDFtitle: 'pozvánka a rozpis závodu',
+                PDF: 'Traskacek 2025.pdf'
+        },
+     
+
+]
+
+export const aktualityImage  = "";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
+
+export default aktuality
---
+++ data/password.ts
@@ -0,0 +1,10 @@
+// odkaz na generovani hashe hesla:
+// https://passwordsgenerator.net/sha256-hash-generator/
+
+
+const correctHashes = [
+    'F1813E12AC248415DB825830729AAB511B68E7221470563ACBC6B62AA2CA3B6F',
+    '87986A616D808890B7643788F2001BE6A29A939D411B11C5DE02D71A128DF20F'
+]
+
+export default correctHashes
