7d97a04c8f7a04d3f8438aafa49082d590cab8e3
admin-trasko <167458084+admin-trasko@users.noreply.github.com>
2025-04-08 07:47:40 +0200

    Update aktuality.ts

--- public/data/aktuality.ts
+++ public/data/aktuality.ts
@@ -3,13 +3,8 @@ import {Aktualita} from "../../src/interfaces/interfaces";
 const aktuality : Aktualita[] = [
         {
                 title: 'O pohár města Vyškova ',
-                PDFtitle: 'pozvánka a rozpis závodu',
-                PDF: 'O pohar mesta Vyskova 2025.pdf'
-        },
-        {
-                title: 'O pohár města Vyškova ',
-                PDFtitle: 'časový program a rozcvičení',
-                PDF: 'O pohar mesta Vyskova 2025 - časový program.pdf'
+                PDFtitle: 'výsledky',
+                PDF: 'Vyškovský pohár 2025 - výsledky.pdf'
         },
         {
                 title: 'Traskáček ',
