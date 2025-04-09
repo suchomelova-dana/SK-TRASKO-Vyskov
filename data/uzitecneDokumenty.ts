86c57ad005b431f4004a74e2288dea4a1b74a496
suchomelova-dana <danka.suchomelova@seznam.cz>
2025-04-09 05:29:13 +0000

    gh-pages

---
+++ data/uzitecneDokumenty.ts
@@ -0,0 +1,23 @@
+import {Document} from "../../src/interfaces/interfaces";
+
+const uzitecneDokumenty: Document[] = [
+    {
+        title: 'Normy náčiní',
+        document: 'nacini.pdf'
+    },
+    {
+        title: 'Rozvrh jaro 2025',
+        document: 'rozvrh jaro 2025.pdf'
+    },
+    {
+        title: 'Provozní řád domečku',
+        document: 'provozni_rad_domecku.pdf'
+    },
+    {
+        title: 'Prohlídka od pediatra',
+        document: 'zpusobilost_sport.pdf'
+    },
+
+]
+
+export default uzitecneDokumenty
