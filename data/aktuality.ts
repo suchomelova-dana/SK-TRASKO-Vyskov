90796949d0cc465628904c0feb41618fb30d8443
suchomelova-dana <danka.suchomelova@seznam.cz>
2025-06-10 13:34:16 +0000

    gh-pages

---
+++ data/aktuality.ts
@@ -0,0 +1,20 @@
+import {Aktualita} from "../../src/interfaces/interfaces";
+
+const aktuality : Aktualita[] = [
+        {
+                title: 'Družstvo seniorek estetické skupinové gymnastiky STORM skončilo 12. na Světovém poháru v Torontu! ',
+                PDFtitle: '',
+                PDF: ''
+        },
+        {
+                title: 'Traskáček ',
+                PDFtitle: 'pozvánka a rozpis závodu',
+                PDF: 'Traskacek 2025.pdf'
+        },
+     
+
+]
+
+export const aktualityImage  = "Seniorky - Toronto.jpg";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
+
+export default aktuality
