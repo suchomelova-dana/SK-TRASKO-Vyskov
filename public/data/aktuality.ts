7c372ba2119a01aedb7c57844d6e7bf78873f27e
admin-trasko <167458084+admin-trasko@users.noreply.github.com>
2025-06-16 09:56:46 +0200

    Update aktuality.ts

--- public/data/aktuality.ts
+++ public/data/aktuality.ts
@@ -2,19 +2,19 @@ import {Aktualita} from "../../src/interfaces/interfaces";
 
 const aktuality : Aktualita[] = [
         {
-                title: 'Družstvo seniorek estetické skupinové gymnastiky STORM skončilo 12. na Světovém poháru v Torontu! ',
+                title: 'Všechny příznivce moderní gymnastiky zveme na 21. Akademii oddílu ',
                 PDFtitle: '',
                 PDF: ''
         },
         {
-                title: 'Traskáček ',
-                PDFtitle: 'pozvánka a rozpis závodu',
-                PDF: 'Traskacek 2025.pdf'
+                title: 'Oddíl SK TRASKO Vyškov se zúčastní Vyškovského festivalu sportu - Park v pohybu 20. 6. 2025. Najdete nás na stanovišti na ploše fotbalového hriště ',
+                PDFtitle: '',
+                PDF: ''
         },
      
 
 ]
 
-export const aktualityImage  = "Seniorky - Toronto.jpg";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
+export const aktualityImage  = "sk trasko vyskov_akademie 2025_pozvanka A5-001.jpg";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
 
 export default aktuality
