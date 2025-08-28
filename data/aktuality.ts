cf755178fee33160ae10578d9193077907876e7f
suchomelova-dana <danka.suchomelova@seznam.cz>
2025-08-28 08:12:19 +0000

    gh-pages

---
+++ data/aktuality.ts
@@ -0,0 +1,20 @@
+import {Aktualita} from "../../src/interfaces/interfaces";
+
+const aktuality : Aktualita[] = [
+        {
+                title: 'Všechny příznivce moderní gymnastiky zveme na 21. Akademii oddílu ',
+                PDFtitle: '',
+                PDF: ''
+        },
+        {
+                title: 'Oddíl SK TRASKO Vyškov se zúčastní Vyškovského festivalu sportu - Park v pohybu 20. 6. 2025. Najdete nás na stanovišti na ploše fotbalového hriště ',
+                PDFtitle: '',
+                PDF: ''
+        },
+     
+
+]
+
+export const aktualityImage  = "sk trasko vyskov_akademie 2025_pozvanka A5-001.jpg";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
+
+export default aktuality
