df6ce0cb5fc99b9748b809dacdbabe9e09531053
suchomelova-dana <danka.suchomelova@seznam.cz>
2025-09-04 12:37:50 +0000

    gh-pages

---
+++ data/clenkySA.ts
@@ -0,0 +1,16 @@
+import {Member} from "../../src/interfaces/interfaces";
+
+const SAmembers : Member[] = [
+    { name: 'Frydrychová Eliška' },
+    { name: 'Hrdličková Aneta' },
+    { name: 'Mikešová Alexandra' },
+    { name: 'Němcová Melánie' },
+    { name: 'Novosadová Evelin' },
+    { name: 'Pecháčková Sára' },
+    { name: 'Sochorová Zuzana' },
+    { name: 'Šmakalová Iveta' },
+    { name: 'Tesařová Gabriela' },
+    { name: 'Vejrostová Laura' },
+]
+
+export default SAmembers
