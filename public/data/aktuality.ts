92177ce5f28ae6548d76143b9bcc6c2c8bdc3d42
admin-trasko <167458084+admin-trasko@users.noreply.github.com>
2025-09-04 14:22:29 +0200

    Update aktuality.ts

--- public/data/aktuality.ts
+++ public/data/aktuality.ts
@@ -2,17 +2,11 @@ import {Aktualita} from "../../src/interfaces/interfaces";
 
 const aktuality : Aktualita[] = [
         {
-                title: 'Všechny příznivce moderní gymnastiky zveme na 21. Akademii oddílu ',
+                title: 'Trasko Cup - pozvánka a rozpis závodu ',
                 PDFtitle: '',
-                PDF: ''
+                PDF: 'TRASKO CUP 2025 - rozpis.pdf'
         },
-        {
-                title: 'Oddíl SK TRASKO Vyškov se zúčastní Vyškovského festivalu sportu - Park v pohybu 20. 6. 2025. Najdete nás na stanovišti na ploše fotbalového hriště ',
-                PDFtitle: '',
-                PDF: ''
-        },
-     
-
+        
 ]
 
 export const aktualityImage  = "sk trasko vyskov_akademie 2025_pozvanka A5-001.jpg";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
