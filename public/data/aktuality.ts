60b8e46fcccf8e415987640052c1b10aba28ae26
admin-trasko <167458084+admin-trasko@users.noreply.github.com>
2026-02-25 15:11:08 +0100

    Update aktuality.ts

--- public/data/aktuality.ts
+++ public/data/aktuality.ts
@@ -6,6 +6,11 @@ const aktuality : Aktualita[] = [
                 PDFtitle: 'výsledky závodu',
                 PDF: 'KZ I 2026 - vysledky.pdf',
         },
+        {
+                title: 'Kontrolní závod II.',
+                PDFtitle: 'časový program',
+                PDF: 'KZII 2026-casovy program.pdf',
+        },
         
 ]
 
