026c59edb070a64b539c1c4ec220fc762254ec48
suchomelova-dana <danka.suchomelova@seznam.cz>
2026-03-09 13:53:16 +0000

    gh-pages

---
+++ data/aktuality.ts
@@ -0,0 +1,28 @@
+import {Aktualita} from "../../src/interfaces/interfaces";
+
+const aktuality : Aktualita[] = [
+        {
+                title: 'Kontrolní závod I.',
+                PDFtitle: 'výsledky závodu',
+                PDF: 'KZ I 2026 - vysledky.pdf',
+        },
+        {
+                title: 'Kontrolní závod II.',
+                PDFtitle: 'výsledky závodu',
+                PDF: 'KZ II 2026 - výsledky.pdf',
+        },
+        {
+                title: 'Kontrolní závod III.',
+                PDFtitle: 'výsledky závodu',
+                PDF: 'KZ III 2026 - výsledky.pdf',
+        },
+        {
+                title: 'Mezioddílový závod',
+                PDFtitle: 'časový program',
+                PDF: 'MZ Vyskov - casovy program.pdf',
+        },
+]
+
+export const aktualityImage  = "";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
+
+export default aktuality
