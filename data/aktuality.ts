bba8a3a05274e00a88d17aeb9d4341b86002a0af
suchomelova-dana <danka.suchomelova@seznam.cz>
2026-04-17 13:31:37 +0000

    gh-pages

---
+++ data/aktuality.ts
@@ -0,0 +1,28 @@
+import {Aktualita} from "../../src/interfaces/interfaces";
+
+const aktuality : Aktualita[] = [
+        {
+                title: 'Mezioddílový závod',
+                PDFtitle: 'výsledky závodu',
+                PDF: 'MZ 2026 - vysledky.pdf',
+        },
+        {
+                title: 'Mezioddílový závod v Brně',
+                PDFtitle: 'výsledky závodu',
+                PDF: 'MZ Mantila 2026 - výsledky.pdf',
+        },
+        {
+                title: 'O pohár města Vyškova',
+                PDFtitle: 'časový program',
+                PDF: 'O pohar mesta Vyskova 2026 - časový program.pdf',
+        },
+        {
+                title: 'ESG Cup IV - Vyškov',
+                PDFtitle: 'časový program',
+                PDF: 'ESG CUP IV - časový program.pdf',
+        },
+]
+
+export const aktualityImage  = "";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
+
+export default aktuality
