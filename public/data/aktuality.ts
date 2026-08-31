4b077a4a6b58845f6e006472a4d6e66ad8592ef5
admin-trasko <167458084+admin-trasko@users.noreply.github.com>
2026-08-31 11:21:05 +0200

    Update aktuality.ts

--- public/data/aktuality.ts
+++ public/data/aktuality.ts
@@ -11,18 +11,9 @@ const aktuality : Aktualita[] = [
                 PDFtitle: 'výsledky závodu',
                 PDF: 'MZ Mantila 2026 - výsledky.pdf',
         },
-        {
-                title: 'O pohár města Vyškova',
-                PDFtitle: 'časový program',
-                PDF: 'O pohar mesta Vyskova 2026 - časový program.pdf',
-        },
-        {
-                title: 'ESG Cup IV - Vyškov',
-                PDFtitle: 'časový program',
-                PDF: 'ESG CUP IV - časový program.pdf',
-        },
+       
 ]
 
-export const aktualityImage  = "";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
+export const aktualityImage  = "talentovky leták 2026.jpg";         // jmeno souboru i s příponou, který vložíš do /obrazky/akuality
 
 export default aktuality
