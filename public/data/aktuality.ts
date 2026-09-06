740ffe9b720c8b90acb75d3aea887c1c5e3c6294
admin-trasko <167458084+admin-trasko@users.noreply.github.com>
2026-09-06 19:56:02 +0200

    Update aktuality.ts

--- public/data/aktuality.ts
+++ public/data/aktuality.ts
@@ -2,9 +2,11 @@ import {Aktualita} from "../../src/interfaces/interfaces";
 
 const aktuality : Aktualita[] = [
         {
-                title: 'Mezioddílový závod',
-                PDFtitle: 'výsledky závodu',
-                PDF: 'MZ 2026 - vysledky.pdf',
+                title: 'Zveme malá šikovná děvčátka vyzkoušet si trénink moderní gymnastiky. 
+Máme posledních několik volných míst v gymnastické přípravce a gymnastické školičce. Až do 15. 10. nabízíme možnost přijít si vyzkoušet cvičení opravdové moderní gymnastiky. https://docs.google.com/forms/d/e/1FAIpQLScsdljfQHb6UHLs3CErXOExS5FvvLi11veAfXBdvuuCCZBNYg/viewform
+',
+                PDFtitle: '',
+                PDF: '',
         },
        
 ]
