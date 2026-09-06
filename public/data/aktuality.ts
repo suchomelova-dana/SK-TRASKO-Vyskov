c2050f12c8026be9f767ab6fa6d03e24a1ccb187
admin-trasko <167458084+admin-trasko@users.noreply.github.com>
2026-09-06 19:58:23 +0200

    Update aktuality.ts

--- public/data/aktuality.ts
+++ public/data/aktuality.ts
@@ -3,8 +3,7 @@ import {Aktualita} from "../../src/interfaces/interfaces";
 const aktuality : Aktualita[] = [
         {
                 title: 'Zveme malá šikovná děvčátka vyzkoušet si trénink moderní gymnastiky. 
-Máme posledních několik volných míst v gymnastické přípravce a gymnastické školičce. Až do 15. 10. nabízíme možnost přijít si vyzkoušet cvičení opravdové moderní gymnastiky. https://docs.google.com/forms/d/e/1FAIpQLScsdljfQHb6UHLs3CErXOExS5FvvLi11veAfXBdvuuCCZBNYg/viewform
-',
+Máme posledních několik volných míst v gymnastické přípravce a gymnastické školičce. Až do 15. 10. nabízíme možnost přijít si vyzkoušet cvičení opravdové moderní gymnastiky. ',
                 PDFtitle: '',
                 PDF: '',
         },
