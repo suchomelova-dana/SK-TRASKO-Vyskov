1807812ee7cf95eaefd7d32ef7c9293d3081d8bb
admin-trasko <167458084+admin-trasko@users.noreply.github.com>
2026-09-07 08:29:34 +0200

    Update aktuality.ts

--- public/data/aktuality.ts
+++ public/data/aktuality.ts
@@ -7,10 +7,10 @@ const aktuality : Aktualita[] = [
                 PDF: ''
         },
         {
-                title: 'Nestihli jste talentové zkoušky? Nevadí.',
+                title: 'Nestihli jste talentové zkoušky? Nevadí. Přijďte se podívat přímo na trénink.',
                 PDFtitle: '',
                 PDF: '',
-                link: 'https://docs.google.com/forms/d/e/1FAIpQLScsdljfQHb6UHLs3CErXOExS5FvvLi11veAfXBdvuuCCZBNYg/viewform',
+                link: 'https://https://docs.google.com/forms/d/e/1FAIpQLScsdljfQHb6UHLs3CErXOExS5FvvLi11veAfXBdvuuCCZBNYg/viewform',
                 linkTitle: 'Vyplnit nezávaznou přihlášku'
         },
        
